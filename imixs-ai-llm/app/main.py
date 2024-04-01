8bc94cc54d8961c226dcc942fb067ec5487823e1
Ralph Soika <ralph.soika@imixs.com>
2024-04-01 10:33:37 +0200

    update

--- imixs-ai-llm/app/main.py
+++ imixs-ai-llm/app/main.py
@@ -16,31 +16,12 @@ app = FastAPI(title="FastAPI::XML", default_response_class=XmlAppResponse)
 app.router.route_class = XmlRoute
 add_openapi_extension(app)
 
-# Init Model
-print("Init Model...")
-model_path = "/models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
+model = None
+
+#model_path = "/models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
 model_path = "/models/mistral-7b-instruct-v0.2.Q4_K_S.gguf"
-#model_path = "/models/mistral-7b-instruct-v0.2.Q3_K_M.gguf"
-
-# Create a llama model
-# Find API Reference: https://llama-cpp-python.readthedocs.io/en/latest/api-reference/
-#  n_gpu_layers=30
-model = Llama(
-    model_path=model_path,
-    temperature=0.1,
-    max_tokens=200,
-    n_ctx=3048,
-    ctx_size=3000,
-    seed=-1, 
-    n_threads=8,
-    verbose=True,
-    echo=False,
-    n_gpu_layers=30
-)
 
 
-
-print("Init Model...finished.")
 
 #####################
 # Basis prompt method. This method expects a XMLPrompt dataobject holding the system and user message
@@ -55,7 +36,29 @@ print("Init Model...finished.")
 #
 @app.post("/prompt", response_model=datamodel.XMLPrompt, tags=["Imixs-AI"])
 def prompt(data: datamodel.XMLPrompt = XmlBody()) -> datamodel.XMLPrompt:
+
+
+    # Create a llama model if not yet initialized
+    if model is None :
+        print("--- Init Model...")
+        model = Llama(
+            model_path=model_path,
+            temperature=0.1,
+            max_tokens=200,
+            n_ctx=3048,
+            ctx_size=3000,
+            seed=-1, 
+            n_threads=8,
+            verbose=True,
+            echo=False,
+            n_gpu_layers=30
+        )
+        print("--- Init Model...finished!")
+
+
+
     # Model parameters
+    print("--- compute prompt....")
     max_tokens = 2000
     prompt = f"""<s>[INST] {data.instruction} [/INST] {data.context} """
     print("start processing prompt:\n\n",prompt,'\n...\n')
@@ -64,25 +67,6 @@ def prompt(data: datamodel.XMLPrompt = XmlBody()) -> datamodel.XMLPrompt:
                      echo=False
                      )
     return data;
-
-
-# Nicht so gut
-@app.post("/prompt2", response_model=datamodel.XMLPrompt, tags=["Imixs-AI"])
-def prompt(x: datamodel.XMLPrompt = XmlBody()) -> datamodel.XMLPrompt:
-
-
-    # Model parameters
-    max_tokens = 2000
-
-
-    prompt = f"""<s>[INST] {x.context} [INST]</s>[INST] {x.instruction} [/INST] """
-    print("start processing prompt:\n\n",prompt,'\n...\n')
-    x.output = model(prompt, max_tokens=max_tokens, 
-                     temperature=0,
-                     echo=False
-                     )
-
-    return x;
 
 
 
