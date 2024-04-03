09f7293f7d1762f3f4994ffa9f18db26a27cde1e
Ralph Soika <ralph.soika@imixs.com>
2024-04-03 23:03:36 +0200

    rollback

--- imixs-ai-llm/app/imixs/core/datamodel.py
+++ imixs-ai-llm/app/imixs/core/datamodel.py
@@ -7,10 +7,42 @@ The .....
 # This package provides data classes used to exchange prompt and configuration data.
 #
 from pydantic import BaseModel
-
+from dataclasses import dataclass, field
+from typing import List
+import json
 
 
 class PromptEntity(BaseModel):
     instruction: str
     context: str
- 
+   
+##########################################################
+# This dataclass is used to exchange the prompt data between the rest api and the LLM.
+# 
+# The class uses the dataclass decorator (introduced in Python 3.7) that allows an easy definition of classes that only store data, 
+# similar to NamedTuples, but with additional features and a familiar class syntax.
+##########################################################
+@dataclass
+class XMLPrompt:
+    instruction: str = field(
+        metadata={
+            "example": "What is the Imixs-Workflow engine?",
+            "name": "instruction", 
+            "type": "Element"
+        }
+    )    
+    context: str = field(
+        metadata={
+            "example": "You are a helpful code assistant.",
+            "name": "context", 
+            "type": "Element"
+        }
+    )
+
+
+    output: str = field(
+        metadata={
+            "name": "output", 
+            "type": "Element"
+        }
+    )    
