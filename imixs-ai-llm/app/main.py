aa7e120220197fbdaa935fe05730a4de71cf6521
Ralph Soika <ralph.soika@imixs.com>
2024-04-03 22:11:16 +0200

    test

--- imixs-ai-llm/app/main.py
+++ imixs-ai-llm/app/main.py
@@ -92,3 +92,16 @@ def prompt(x: datamodel.XMLPrompt = XmlBody()) -> datamodel.XMLPrompt:
     print(x.output)
     return x;
 
+
+@app.get("/simple")
+async def test_get():
+
+    print("--start simple test --")
+    llm = Llama(model_path=model_path, n_gpu_layers=30, n_ctx=3584, n_batch=521, verbose=True)
+    # adjust n_gpu_layers as per your GPU and model
+    output = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, stop=["Q:", "\n"], echo=True)
+    print(output)
+
+    return {"message": "Hello World"}
+
+
