7aa7c00f0688606a0e7933736efd9c5f0d3da8fe
Ralph Soika <ralph.soika@imixs.com>
2024-04-05 10:31:13 +0200

    update

--- imixs-ai-exllama/README.md
+++ imixs-ai-exllama/README.md
@@ -5,8 +5,60 @@
 You can find the exlv2 model variants for Mistral-7B on hugging face: https://huggingface.co/turboderp/Mistral-7B-instruct-exl2
 
 
+Model Sources:
+
+ - https://huggingface.co/alokabhishek/Mistral-7B-Instruct-v0.2-5.0-bpw-exl2
+
+
+
+# Download Mistral 7B Model
+
+
+Quick Install of git-lfs:
+
+
+    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+
+
+To download the Mistral 7B Model from [huggingface.co](https://huggingface.co/turboderp/Mistral-7B-instruct-exl2/tree/4.0bpw): 
+
+# https://huggingface.co/turboderp/Mistral-7B-instruct-exl2/tree/4.0bpw
+```
+$ sudo apt install python3-pip python3.11-venv -y
+$ cd
+$ source .env/bin/activate
+$ pip install --upgrade huggingface_hub
+$ huggingface-cli download turboderp/Mistral-7B-instruct-exl2/tree/4.0bpw 4.0bpw --local-dir . --local-dir-use-symlinks False
+```
+
+Next make sure the two models are located unter `imixs-ai/imixs-ai-llm/models`
+
+```
+$ cd 
+$ cd imixs-ai/imixs-ai-llm
+$ mkdir models
+$ cd models
+```
+
+
+
+
+from huggingface_hub import hf_hub_download repo_id = "turboderp/Mistral-7B-instruct-exl2" directory_name = "tree/4.0bpw 4.0bpw" download_path = hf_hub_download(repo_id=repo_id, filename=directory_name) 
+
+
+
 # Docker
 
 We provide a Dockerfile using GPU via CUDA. To build the Docker file run:
 
-    $ docker build . -f ./Dockerfile -t imixs/imixs-ai-exllama
+    $ docker build . -t imixs/imixs-ai-exllama
+
+
+
+Run
+
+
+    $ docker run --gpus all -v ./models:/models   imixs/imixs-ai-exllama
+
+
+
--- imixs-ai-exllama/app/main.py
+++ imixs-ai-exllama/app/main.py
@@ -1,112 +1,56 @@
-from typing import Union
-from dataclasses import dataclass, field
-from fastapi import FastAPI
-from fastapi_xml import add_openapi_extension
-from fastapi_xml import XmlRoute
-from fastapi_xml import XmlAppResponse
-from fastapi_xml import XmlBody
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from llama_cpp import Llama
-from imixs.core import datamodel
+from exllamav2 import(
+    ExLlamaV2,
+    ExLlamaV2Config,
+    ExLlamaV2Cache,
+    ExLlamaV2Tokenizer,
+)
+
+from exllamav2.generator import (
+    ExLlamaV2BaseGenerator,
+    ExLlamaV2Sampler
+)
+
 import time
 
-# Setup FastAPI with the default XMLAPPResponse class
-# 
-# app = FastAPI()
-app = FastAPI(title="FastAPI::XML", default_response_class=XmlAppResponse)
-app.router.route_class = XmlRoute
-add_openapi_extension(app)
+# Initialize model and cache
 
-model = None
+model_directory =  "/models/Mistral-7B-Instruct-v0.2-5.0-bpw-exl2/"
+print("Loading model: " + model_directory)
 
-model_path = "/models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
-#model_path = "/models/mistral-7b-instruct-v0.2.Q4_K_S.gguf"
+config = ExLlamaV2Config(model_directory)
+model = ExLlamaV2(config)
+cache = ExLlamaV2Cache(model, lazy = True)
+model.load_autosplit(cache)
+tokenizer = ExLlamaV2Tokenizer(config)
 
+# Initialize generator
 
+generator = ExLlamaV2BaseGenerator(model, cache, tokenizer)
 
-#####################
-# Basis prompt method. This method expects a XMLPrompt dataobject holding the system and user message
-# The output is stored in the tag 'output'.
-#
-# Example: 
-# <XMLPrompt>
-#	<system_message>Du bist ein hilfreicher Java Code Assistent.</system_message>
-#	<user_message>Was ist die Imixs-Workflow engine?</user_message>
-#   <output></output>
-# </XMLPrompt>
-#
-# Note: Option 'logits_all=True' is important here because of bug: https://github.com/abetlen/llama-cpp-python/issues/1326
+# Generate some text
 
-@app.post("/prompt", response_model=datamodel.XMLPrompt, tags=["Imixs-AI"])
-def prompt(data: datamodel.XMLPrompt = XmlBody()) -> datamodel.XMLPrompt:
+settings = ExLlamaV2Sampler.Settings()
+settings.temperature = 0.85
+settings.top_k = 50
+settings.top_p = 0.8
+settings.token_repetition_penalty = 1.01
+settings.disallow_tokens(tokenizer, [tokenizer.eos_token_id])
 
-    global model
+prompt = "Our story begins in the Scottish town of Auchtermuchty, where once"
 
-    # Create a llama model if not yet initialized
-    if model is None :
+max_new_tokens = 150
 
-        start_time = time.time()
-        print("--- Init Model...")
-        model = Llama(
-            model_path=model_path,
-            n_gpu_layers=30, 
-            n_ctx=3584, 
-            n_batch=521, 
-            verbose=True,
-            logits_all=True,
-            echo=False
-        )
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(f"--- Init Model...finished in {execution_time} sec")
+generator.warmup()
+time_begin = time.time()
 
+output = generator.generate_simple(prompt, settings, max_new_tokens, seed = 1234)
 
+time_end = time.time()
+time_total = time_end - time_begin
 
-    # Model parameters
-    print("--- compute prompt....")
-    max_tokens = 2000
-    prompt = f"""<s>[INST] {data.instruction} [/INST] {data.context} """
-    print("start processing prompt:\n\n",prompt,'\n...\n')
-    result = model(prompt, max_tokens=max_tokens, 
-                     temperature=0,
-                     echo=False
-                     )
-    print(result)
-    data.output = result
-    return data;
-
-
-
-
-@app.get("/simple")
-async def test_get():
-
-    data = datamodel.XMLPrompt(
-        instruction="",
-        context="",
-        output=""
-    )
-
-    print("--start simple test --")
-    llm = Llama(model_path=model_path, n_gpu_layers=30, n_ctx=3584, n_batch=521, verbose=True)
-    # adjust n_gpu_layers as per your GPU and model
-    output = llm("Q: Name and explain the planets in the solar system? A: ", max_tokens=2000, stop=["Q:", "\n"], echo=True)
-    print(output)
-
-    data.output = output
-    return data;
-
-
-#@app.post("/simplepost")
-#async def test_simplepost(prompt: datamodel.PromptEntity):
-#
-#    print("--start simple test --")
-#    llm = Llama(model_path=model_path, n_gpu_layers=30, n_ctx=3584, n_batch=521, verbose=True)
-#    # adjust n_gpu_layers as per your GPU and model
-#    # <s>[INST] You are a helpful Java Developer. [/INST] Explain me the Imixs-Workflow engine.
-#    output = llm(prompt.instruction, max_tokens=2000, stop=["Q:", "\n"], echo=True)
-#    print(output)
-#
-#    return {"message": "Hello World"}
-
-
+print(output)
+print()
+print(f"Response generated in {time_total:.2f} seconds, {max_new_tokens} tokens, {max_new_tokens / time_total:.2f} tokens/second")
