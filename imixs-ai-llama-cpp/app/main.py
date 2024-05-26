94be78f1b5cbc9872c66914fcf76fcec8ae61330
Ralph Soika <ralph.soika@imixs.com>
2024-05-26 15:45:38 +0200

    improvements
    Issue #16

--- imixs-ai-llama-cpp/app/main.py
+++ imixs-ai-llama-cpp/app/main.py
@@ -18,9 +18,10 @@ app = FastAPI(title="FastAPI::XML", default_response_class=XmlAppResponse)
 app.router.route_class = XmlRoute
 add_openapi_extension(app)
 
-model = None
 model_path = "/models/"
-model_id= "mistral-7b-instruct-v0.2.Q3_K_M.gguf"
+model_options = None
+model = "mistral-7b-instruct-v0.2.Q3_K_M.gguf"
+llm = None
 
 #####################
 # Basis prompt method. This method expects a PromptDefinition dataobject holding the modelid and the prompt  message
@@ -38,51 +39,56 @@ def prompt(data: datamodel.PromptDefinition = XmlBody()) -> datamodel.PromptDefi
 
     global llm
     global model
+    global model_options
 
-    # Create a llama model if not yet initialized
-    if model is None or (data.model != '' and data.model != model):
-        print("--- Init Model...")
-        if data.model != '' :
-            model=data.model
-        print("-- Model Path = "+model_path+model)
-
-        # convert the options JSON-String into a dict....
-        print(data.model_options)
-        model_options = json.loads(data.model_options)
+    # Init model (using a cache)
+    llm=initModel(data.model,data.model_options);
     
-        # llm = Llama(
-        #     model_path=model_path+model,
-        #     # 30, -1
-        #     n_gpu_layers=-1, 
-        #     #n_ctx=3584, 
-        #     #n_ctx=4096, 
-        #     #n_ctx=5120, 
-        #     n_ctx=8192, 
-        #     #n_batch=521, 
-        #     #verbose=True,
-        #     logits_all=True,
-        #     echo=False
-        # )
-        llm = Llama(
-            model_path=model_path+model,
-            **model_options
-        )
+    print("start processing \n\n")
+    print("      model_options=",model_options,'\n')
 
-    max_tokens = 4096
-    print("start processing prompt:\n\n",data.prompt,'\n...\n')
 
     # Convert the prompt_options...
     print(data.prompt_options)
     prompt_options = json.loads(data.prompt_options)
     
-    # result = llm(data.prompt, max_tokens=max_tokens, 
-    #                  temperature=0,
-    #                  echo=False
-    #                  )
+    print("      prompt_options=\n",prompt_options,'\n')
+    print("      prompt=\n",data.prompt,'\n\n')    
     result = llm(data.prompt, 
                  **prompt_options
     )    
     print(result)
     resultData = datamodel.ResultData(result["choices"][0]["text"])
     return resultData;
+
+
+#####################
+# This helper method initializes the new llm instance with the given modelid and model options.
+# 
+# The method uses a caching mechanism to initialize the model only if not yet done. 
+# For this reason the model name and the model options are stored in global variables
+# which allows us to verify if model or options have changed during the last call.
+######################
+def initModel(_model: str, _model_options: str):
+
+    global llm
+    global model
+    global model_options
+
+    # Convert options string into a dict...
+    model_options_dict=json.loads(_model_options)
+
+    # Verify if the model or options have changed?
+    if (llm is None) or (model != _model) or (model_options != model_options_dict):
+        model_options=model_options_dict
+        model=_model
+        # create a new Llama instance
+        print("--- Init new llm - Model ID = "+model)
+        llm = Llama(
+            model_path=model_path+model,
+            **model_options
+        )
+    
+    return llm
+
 
